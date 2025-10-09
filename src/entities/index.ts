d87bddd9e401d65a1c36c4093f3f9b48a6c7a390
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-08 22:09:17 -0500

    feat: expand barrel exports in entities layer
    - Added exports for district, driver, vehicle, and shared modules to `index.ts` for streamlined module imports.

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,4 +1,5 @@
 // Entities Layer - Main barrel export
+
 // User Entity exports
 export * from './user/model';
 export * from './user/api';
@@ -6,3 +7,18 @@ export * from './user/api';
 // Role Entity exports
 export * from './role/model';
 export * from './role/api';
+
+// District Entity exports
+export * from './district/model';
+export * from './district/api';
+
+// Driver Entity exports
+export * from './driver/model';
+export * from './driver/api';
+
+// Vehicle Entity exports
+export * from './vehicle/model';
+export * from './vehicle/api';
+
+// Shared exports
+export * from '../shared/api';
