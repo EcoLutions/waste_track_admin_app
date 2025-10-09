a6e8d3d6202ee105f1d02ae12d3cd1160d0c345a
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-08 21:07:42 -0500

    feat: add environment configuration files for production and development
    - Introduced `environment.ts` for production setup.
    - Added `environment.development.ts` for development with debug tools and detailed logs enabled.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:9500/api/v1/',
+  enableDebugTools: true,
+  logLevel: 'debug'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:9500/api/v1/',
+  enableDebugTools: false,
+  logLevel: 'error'
+};
