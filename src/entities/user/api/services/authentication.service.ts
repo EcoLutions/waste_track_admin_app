649a19169dd101af113005fe7af4343d83c8b906
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-12 13:27:54 -0500

    feat(auth): add password initialization and reset endpoints
    - Added `setInitialPassword` method to define an initial user password using an activation token.
    - Implemented `resetPassword` method for resetting passwords with a reset token.
    - Included `forgotPassword` method for initiating password recovery via email.
    - Introduced new request types: `SetInitialPasswordRequestType` and `ResetPasswordRequestType`.

--- src/entities/user/api/services/authentication.service.ts
+++ src/entities/user/api/services/authentication.service.ts
@@ -10,6 +10,9 @@ import {
   SignInRequestFromCredentialsMapper
 } from '../mappers/sign-in-request-from-credentials.mapper';
 import {catchError} from 'rxjs/operators';
+import {SetInitialPasswordRequestType} from '../types/set-initial-password-request.type';
+import {ResetPasswordRequestType} from '../types/reset-password-request.type';
+import {HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -36,5 +39,35 @@ export class AuthenticationService extends BaseService {
       retry(2),
       catchError(this.handleError)
     );
+  }
+
+  setInitialPassword(accessToken: string, newPassword: string): Observable<void> {
+    const request: SetInitialPasswordRequestType = {
+      activationToken: accessToken,
+      password: newPassword
+    };
+
+    return this.http.post<void>(`${this.resourcePath()}/set-initial-password`, request, this.httpOptions).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+
+  forgotPassword(email: string): Observable<void> {
+    const params = new HttpParams().set('email', email);
+    return this.http.post<void>(`${this.resourcePath()}/forgot-password`, null,  { ...this.httpOptions, params: params }).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+
+  resetPassword(token: string, newPassword: string): Observable<void> {
+    const request: ResetPasswordRequestType = {
+      resetToken: token,
+      newPassword: newPassword
+    };
+    return this.http.post<void>(`${this.resourcePath()}/reset-password`, request , this.httpOptions).pipe(
+      retry(2),
+    )
   }
 }
