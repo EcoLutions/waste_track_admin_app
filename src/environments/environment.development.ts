88032932dd985344cf3e4de24737921dbcdd2873
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-15 14:54:52 -0500

    google keys

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,9 +2,13 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api/v1/',
   googleMaps: {
-    apiKey: '',
+    apiKey: 'AIzaSyCNC6IN3TDO0SyxOuna_bWteD00CqEhpps',
     version: 'weekly',
-    libraries: ['places', 'marker'] as const
+    libraries: ['places', 'marker'] as const,
+    mapIds: {
+      depot: '43b263c8b53f536b75a7140b',
+      disposal: '43b263c8b53f536b75a7140b'
+    }
   },
   enableDebugTools: true,
   logLevel: 'debug'
