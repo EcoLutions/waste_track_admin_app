7f7d81a663db0c87c95a1a8cd5eafbe52544394c
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-12-02 01:16:35 -0500

    refactor(active-routes): enhance route management and loading logic
    Refactor active routes state to include containers and route directions.
    Improve loading methods for active routes and directions, ensuring better
    error handling and state management. Simplify computed properties for
    filtered routes and selected route handling.

--- src/entities/waypoint/api/services/waypoint.service.ts
+++ src/entities/waypoint/api/services/waypoint.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, map, retry } from 'rxjs';
-import { BaseService } from '../../../../shared/api';
+import { BaseService } from '../../../../shared';
 import { WaypointEntity } from '../../model';
 import { WaypointResponse } from '../types/waypoint-response.type';
 import { CreateWaypointRequest } from '../types/create-waypoint-request.type';
